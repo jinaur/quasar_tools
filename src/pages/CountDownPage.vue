c39aa1e4720907325b0fd33fd0cacd36c105b3d3
jinaur <50763720+jinaur@users.noreply.github.com>
2023-08-26 16:26:30 +0900

    3 files modified, 1 added

---
+++ src/pages/CountDownPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <h5>Countdown</h5>
+    <div>
+      <span class="digit">{{ hour }}</span>
+      <span class="digit">{{ min }}</span>
+      <span class="digit">{{ sec }}</span>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const hour = ref(0);
+const min = ref(5);
+const sec = ref(0);
+</script>
+
+<style>
+.digit {
+  border: 1px solid red;
+  margin: 3px;
+}
+</style>
